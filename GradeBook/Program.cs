c77b48adaa9c65d1b4529a6abe18554b9fcadede
Mrduck277 <tinybobross111@gmail.com>
2024-04-05 15:28:31 -0500

    Add project files.

---
+++ GradeBook/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace School
+{
+    public class GradeBook
+    {
+        public static string FirstName()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("please enter first name");
+                    string FirstName = Console.ReadLine();
+                    return FirstName;
+                }
+                catch
+                {
+                    Console.WriteLine("please enter a valid name");
+                    continue;
+                }
+            }
+        }
+
+        private static string LastName() { 
+
+            while (true) { 
+                try
+                {
+                    Console.Write("please enter Last name");
+                    string LastName = Console.ReadLine();
+                    return LastName;
+                }
+                catch
+                {
+                    Console.WriteLine("please enter a valid name");
+                    continue;
+                }
+                   
+            }
+        }
+
+
+        public static void Main(string[] args)
+        {
+            string firstName = FirstName();
+            string lastName = LastName();
+
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+}
+
